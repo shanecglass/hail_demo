db739ebc5101598b4e75f972ed9b49d4174bb81f
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-07-31 16:27:21 -0400

    Add connection iam role

--- bigquery.tf
+++ bigquery.tf
@@ -329,6 +329,7 @@ resource "google_project_iam_member" "dataform_roles" {
   for_each = toset([
     "roles/bigquery.jobUser",                 // Allow Dataform service account to create & execute jobs
     "roles/bigquery.dataViewer",              // Allow Dataform service account to query data
+    "roles/bigquery.connectionUser",          // Allow Dataform to use GCP connection
     "roles/iam.serviceAccountUser"
     ]
   )
