c9b8cb7d982eeefb55aa1ccdbf8911b82ac4ae57
Shane Glass <23001651+shanecglass@users.noreply.github.com>
2023-08-01 10:13:43 -0400

    bug

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "time_sleep" "wait_after_all_resources" {
     google_bigquery_connection.function_connection,
     google_project_iam_member.functions_invoke_roles,
     google_bigquery_table.gcs_objects_hail,
-    google_dataform_repository.cleaning_repo,
+    # google_dataform_repository.cleaning_repo,
     google_project_iam_member.dataform_roles
   ]
 }
